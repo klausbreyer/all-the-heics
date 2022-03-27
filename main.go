f8f70cba329af0809ee0fc61f02f21fa3cc3832f
kb <kb@v01.io>
2022-03-27 15:51:57 +0200

    first working version, doing all at once, without asking for parameters

--- main.go
+++ main.go
@@ -2,66 +2,123 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"image/jpeg"
-	"io"
+	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/adrium/goheif"
 )
 
-// Skip Writer for exif writing
-type writerSkipper struct {
-	w           io.Writer
-	bytesToSkip int
-}
-
-func (w *writerSkipper) Write(data []byte) (int, error) {
-	if w.bytesToSkip <= 0 {
-		return w.w.Write(data)
-	}
-
-	if dataLen := len(data); dataLen < w.bytesToSkip {
-		w.bytesToSkip -= dataLen
-		return dataLen, nil
-	}
-
-	if n, err := w.w.Write(data[w.bytesToSkip:]); err == nil {
-		n += w.bytesToSkip
-		w.bytesToSkip = 0
-		return n, nil
-	} else {
-		return n, err
-	}
-}
-
-func newWriterExif(w io.Writer, exif []byte) (io.Writer, error) {
-	writer := &writerSkipper{w, 2}
-	soi := []byte{0xff, 0xd8}
-	if _, err := w.Write(soi); err != nil {
-		return nil, err
-	}
-
-	if exif != nil {
-		app1Marker := 0xe1
-		markerlen := 2 + len(exif)
-		marker := []byte{0xff, uint8(app1Marker), uint8(markerlen >> 8), uint8(markerlen & 0xff)}
-		if _, err := w.Write(marker); err != nil {
-			return nil, err
-		}
-
-		if _, err := w.Write(exif); err != nil {
-			return nil, err
-		}
-	}
-
-	return writer, nil
-}
-
 func main() {
 	flag.Parse()
 
-	fin, fout := flag.Arg(0), flag.Arg(1)
+	// fin, fout := flag.Arg(0), flag.Arg(1)
+	// convert(fin, fout)
+	home, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	RunDir(fmt.Sprintf("%s/%s", home, "Downloads"))
+}
+
+func RunDir(dir string) {
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatalf("Failed to Read dir: %v\n", err)
+	}
+
+	for _, f := range files {
+		heic := fmt.Sprintf("%s/%s", dir, f.Name())
+		jpg := fmt.Sprintf("%s.jpg", heic)
+
+		if f.IsDir() {
+			RunDir(heic)
+			continue
+		}
+		if isWrongHeic(heic) {
+			rename(heic, jpg)
+			continue
+		}
+
+		if isCorrectHeic(heic) {
+			convert(heic, jpg)
+			modified(heic, jpg)
+			remove(heic)
+			continue
+		}
+	}
+}
+
+//@todo: test.
+func isWrongHeic(fin string) bool {
+	if !hasEnding(fin) {
+		return false
+	}
+	mime := getFileContentType(fin)
+	return mime == "image/jpeg"
+}
+
+//@todo: test.
+func isCorrectHeic(fin string) bool {
+	if !hasEnding(fin) {
+		return false
+	}
+	mime := getFileContentType(fin)
+	return mime == "image/heic" || mime == "image/heif" || mime == "application/octet-stream"
+
+}
+
+//@todo: test.
+
+func hasEnding(fin string) bool {
+	if len(fin) < 6 {
+		return false
+	}
+	return strings.ToLower(fin[len(fin)-4:]) == "heic"
+}
+
+func rename(fin string, fout string) {
+	err := os.Rename(fin, fout)
+	if err != nil {
+		log.Printf("Warning: Cannot rename from %s to %s: %v\n", fin, fout, err)
+	}
+
+	log.Printf("Renamed %s to %s successfully\n", fin, fout)
+}
+
+func modified(fin string, fout string) {
+	file, err := os.Stat(fin)
+
+	if err != nil {
+		log.Printf("Warning: Cannot read file to set original date: %s: %v\n", fin, err)
+	}
+
+	modifiedtime := file.ModTime()
+
+	err = os.Chtimes(fout, modifiedtime, modifiedtime)
+
+	if err != nil {
+		log.Printf("Warning: Cannot change time of %s to %s: %v\n", fin, modifiedtime, err)
+	}
+
+	log.Printf("Modified %s times to: %s", fout, modifiedtime)
+}
+
+func remove(fin string) {
+	err := os.Remove(fin)
+	if err != nil {
+		log.Printf("Warning: Cannot remove %s: %v\n", fin, err)
+	}
+
+	log.Printf("Removed %s successfully\n", fin)
+}
+
+func convert(fin string, fout string) {
 	fi, err := os.Open(fin)
 	if err != nil {
 		log.Fatal(err)
@@ -75,20 +132,23 @@ func main() {
 
 	img, err := goheif.Decode(fi)
 	if err != nil {
-		log.Fatalf("Failed to parse %s: %v\n", fin, err)
-	}
+		log.Printf("Failed to parse %s: %v\n", fin, err)
+		return
 
+	}
 	fo, err := os.OpenFile(fout, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
-		log.Fatalf("Failed to create output file %s: %v\n", fout, err)
+		log.Printf("Failed to create output file %s: %v\n", fout, err)
+		return
 	}
 	defer fo.Close()
 
 	w, _ := newWriterExif(fo, exif)
 	err = jpeg.Encode(w, img, nil)
+
 	if err != nil {
-		log.Fatalf("Failed to encode %s: %v\n", fout, err)
+		log.Printf("Failed to encode %s: %v\n", fout, err)
 	}
 
-	log.Printf("Convert %s to %s successfully\n", fin, fout)
+	log.Printf("Converted %s to %s successfully\n", fin, fout)
 }
---
+++ readme.md
@@ -0,0 +1,10 @@
+# all-the-heics
+
+Scratches my own (and my wife's) itch: Just convert all the *.heic in the download folder of my mac to jpg (while keeping exif and the original timestamp!)
+
+## install
+
+1. `git clone git@github.com:klausbreyer/all-the-heics.git`
+1. `go get -u`
+1. `go build`
+1. drag to dock
